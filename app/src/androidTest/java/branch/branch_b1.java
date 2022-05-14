ecf42647137eadece5b15a1a95a23e05330b2d66
Mousumi269 <fatima1807049@stud.kuet.ac.bd>
2022-05-15 02:04:33 +0600

    changed branch b1. java from main

--- app/src/androidTest/java/branch/branch_b1.java
+++ app/src/androidTest/java/branch/branch_b1.java
@@ -3,11 +3,24 @@ package branch;
 public class branch_b1 {
     int id ;
     String course_name;
+    int values;
 
-    public branch_b1(int id, String course_name) {
+    public void setValues(int values) {
+        this.values = values;
+    }
+
+
+    public int getValues() {
+        return values;
+    }
+
+
+    public branch_b1(int id, String course_name, int values) {
         this.id = id;
         this.course_name = course_name;
+        this.values = values;
     }
+
 
     public int getId() {
         return id;
