b7a19d8148aeb9113fa8be4020dae3b542957dc8
Mousumi269 <fatima1807049@stud.kuet.ac.bd>
2022-05-15 02:34:57 +0600

    Modified b1 from b2

--- app/src/androidTest/java/branch/branch_b1.java
+++ app/src/androidTest/java/branch/branch_b1.java
@@ -4,6 +4,22 @@ public class branch_b1 {
     int id ;
     String course_name;
     String name;
+    int student ;
+
+    public branch_b1(int id, String course_name, String name, int student) {
+        this.id = id;
+        this.course_name = course_name;
+        this.name = name;
+        this.student = student;
+    }
+
+    public int getStudent() {
+        return student;
+    }
+
+    public void setStudent(int student) {
+        this.student = student;
+    }
 
     public void setName(String name) {
         this.name = name;
@@ -14,11 +30,7 @@ public class branch_b1 {
         return name;
     }
 
-    public branch_b1(int id, String course_name, String name) {
-        this.id = id;
-        this.course_name = course_name;
-        this.name = name;
-    }
+
 
 
     public int getId() {
